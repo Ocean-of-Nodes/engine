515dd9ec1bcb6bf7de70f7fc35a5d2ec383c45be
Dmitry <mamhigtt@gmail.com>
2021-06-28 00:43:19 +0700

    impl base build and partial dialect

--- build.rs
+++ build.rs
@@ -1,14 +1,12 @@
-//! Parse and collect inserts into inserts file. 
+//! Parse and collect inserts into inserts file.
 //! After, uses library cmake for building result file as library.
 //!
 //! NOTE: Uses a modified version cpp_build in which the build was removed.
 
 extern crate cpp_build;
-extern crate cmake;
 use std::process::Command;
 
 use cpp_build::{create_lib, CPP_DIR};
-use cmake::Config;
 use std::array;
 
 fn link_libs(libs: &[&str]) {
@@ -19,48 +17,265 @@ fn link_libs(libs: &[&str]) {
 
 fn main() {
     let inserts_path = create_lib("src/main.rs");
-    let lib_path = "/usr/lib/myexternallib";
-    // Config::new(CPP_DIR.as_path())
-    //     .env("LLVM_DIR", env!("LLVM_DIR"))
-    //     .env("MLIR_DIR", env!("MLIR_DIR"))
-    //     .build(); 
     let status = Command::new("cmake")
         .args(&[
-            "-S", "./",
-            "-B", &CPP_DIR.join("build").as_path().to_string_lossy(),
+            "-S",
+            "./",
+            "-B",
+            &CPP_DIR.join("build").as_path().to_string_lossy(),
+            "-G",
+            "Ninja",
         ])
-        // .current_dir(CPP_DIR.as_path())
         .envs(array::IntoIter::new([
             ("LLVM_DIR", env!("LLVM_DIR")),
-            ("MLIR_DIR", env!("MLIR_DIR"))
+            ("MLIR_DIR", env!("MLIR_DIR")),
         ]))
         .status()
-	    .expect("failed to execute cmake");
-	
-	if !status.success() {
-		panic!("cmake execution error: {}", status);
-	} 
+        .expect("failed to execute cmake");
+
+    if !status.success() {
+        panic!("cmake execution error: {}", status);
+    }
 
     let status = Command::new("cmake")
         .current_dir(CPP_DIR.join("build").as_path())
         .args(&["--build", "."])
         .status()
-	    .expect("failed to execute cmake");
+        .expect("failed to execute cmake");
 
     if !status.success() {
         panic!("cmake execution error: {}", status);
-    } 
+    }
 
     let OUT_DIR = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    println!("OUT_DIR: {}", OUT_DIR.to_string_lossy());  
-    println!("cargo:rustc-link-search=native={}", OUT_DIR.to_string_lossy());  
-    
-    println!("cargo:rustc-link-search=native={}", "/home/mrsmith/Desktop/code_experiments/llvm-project/build/lib/"); 
-    
-    // link_libs(&[
+    println!("OUT_DIR: {}", OUT_DIR.to_string_lossy());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        OUT_DIR.to_string_lossy()
+    );
 
-    // ]);
+    println!(
+        "cargo:rustc-link-search=native={}",
+        "/home/mrsmith/Desktop/code_experiments/llvm-project/build/lib/"
+    );
 
-    println!("cargo:rustc-link-lib=static=rust_cpp_generated");
-    println!("cargo:rustc-link-lib=static=stdc++");
+    println!("cargo:rustc-link-lib=dylib=dl");
+    println!("cargo:rustc-link-lib=dylib=m");
+    println!("cargo:rustc-link-lib=dylib=pthread");
+    link_libs(&[
+        "stdc++",
+        // llvm-config --system-libs
+        "rt",
+        "z",
+        "tinfo",
+        // llvm-config --libs
+        "LLVMWindowsManifest",
+        "LLVMXRay",
+        "LLVMLibDriver",
+        "LLVMDlltoolDriver",
+        "LLVMCoverage",
+        "LLVMLineEditor",
+        "LLVMAMDGPUDisassembler",
+        "LLVMAMDGPUAsmParser",
+        "LLVMAMDGPUCodeGen",
+        "LLVMAMDGPUDesc",
+        "LLVMAMDGPUUtils",
+        "LLVMAMDGPUInfo",
+        "LLVMNVPTXCodeGen",
+        "LLVMNVPTXDesc",
+        "LLVMNVPTXInfo",
+        "LLVMX86Disassembler",
+        "LLVMX86AsmParser",
+        "LLVMX86CodeGen",
+        "LLVMX86Desc",
+        "LLVMX86Info",
+        "LLVMOrcJIT",
+        "LLVMMCJIT",
+        "LLVMJITLink",
+        "LLVMInterpreter",
+        "LLVMExecutionEngine",
+        "LLVMRuntimeDyld",
+        "LLVMOrcTargetProcess",
+        "LLVMOrcShared",
+        "LLVMSymbolize",
+        "LLVMDebugInfoPDB",
+        "LLVMDebugInfoGSYM",
+        "LLVMOption",
+        "LLVMObjectYAML",
+        "LLVMMCA",
+        "LLVMMCDisassembler",
+        "LLVMLTO",
+        "LLVMPasses",
+        "LLVMCFGuard",
+        "LLVMCoroutines",
+        "LLVMObjCARCOpts",
+        "LLVMipo",
+        "LLVMVectorize",
+        "LLVMLinker",
+        "LLVMInstrumentation",
+        "LLVMFrontendOpenMP",
+        "LLVMFrontendOpenACC",
+        "LLVMExtensions",
+        "LLVMDWARFLinker",
+        "LLVMGlobalISel",
+        "LLVMMIRParser",
+        "LLVMAsmPrinter",
+        "LLVMDebugInfoMSF",
+        "LLVMDebugInfoDWARF",
+        "LLVMSelectionDAG",
+        "LLVMCodeGen",
+        "LLVMIRReader",
+        "LLVMAsmParser",
+        "LLVMInterfaceStub",
+        "LLVMFileCheck",
+        "LLVMFuzzMutate",
+        "LLVMTarget",
+        "LLVMScalarOpts",
+        "LLVMInstCombine",
+        "LLVMAggressiveInstCombine",
+        "LLVMTransformUtils",
+        "LLVMBitWriter",
+        "LLVMAnalysis",
+        "LLVMProfileData",
+        "LLVMObject",
+        "LLVMTextAPI",
+        "LLVMMCParser",
+        "LLVMMC",
+        "LLVMDebugInfoCodeView",
+        "LLVMBitReader",
+        "LLVMCore",
+        "LLVMRemarks",
+        "LLVMBitstreamReader",
+        "LLVMBinaryFormat",
+        "LLVMTableGen",
+        "LLVMSupport",
+        "LLVMDemangle",
+        //dialect_libs
+        "MLIRAffine",
+        "MLIRAffineEDSC",
+        "MLIRAffineTransforms",
+        "MLIRAffineUtils",
+        "MLIRArmNeon",
+        "MLIRArmSVE",
+        "MLIRArmSVETransforms",
+        "MLIRAsync",
+        "MLIRAsyncTransforms",
+        "MLIRAMX",
+        "MLIRAMXTransforms",
+        "MLIRComplex",
+        "MLIRDLTI",
+        "MLIRGPU",
+        "MLIRLinalgAnalysis",
+        "MLIRLinalgEDSC",
+        "MLIRLinalg",
+        "MLIRLinalgTransforms",
+        "MLIRLinalgUtils",
+        "MLIRLLVMIRTransforms",
+        "MLIRLLVMIR",
+        "MLIRNVVMIR",
+        "MLIRROCDLIR",
+        "MLIRMath",
+        "MLIRMathTransforms",
+        "MLIRMemRef",
+        "MLIRMemRefTransforms",
+        "MLIRMemRefUtils",
+        "MLIROpenACC",
+        "MLIROpenMP",
+        "MLIRPDL",
+        "MLIRPDLInterp",
+        "MLIRQuant",
+        "MLIRSCF",
+        "MLIRSCFTransforms",
+        "MLIRSDBM",
+        "MLIRShape",
+        "MLIRShapeOpsTransforms",
+        "MLIRSPIRV",
+        "MLIRSPIRVModuleCombiner",
+        "MLIRSPIRVConversion",
+        "MLIRSPIRVTransforms",
+        "MLIRSPIRVUtils",
+        "MLIRStandard",
+        "MLIRStandardOpsTransforms",
+        "MLIRTensor",
+        "MLIRTensorTransforms",
+        "MLIRTosa",
+        "MLIRTosaTransforms",
+        "MLIRVector",
+        "MLIRX86Vector",
+        "MLIRX86VectorTransforms",
+        "MLIRTosaTestPasses",
+        //conversion_libs
+        "MLIRAffineToStandard",
+        "MLIRAsyncToLLVM",
+        "MLIRComplexToLLVM",
+        "MLIRGPUToGPURuntimeTransforms",
+        "MLIRGPUToNVVMTransforms",
+        "MLIRGPUToROCDLTransforms",
+        "MLIRGPUToSPIRV",
+        "MLIRGPUToVulkanTransforms",
+        "MLIRLinalgToLLVM",
+        "MLIRLinalgToSPIRV",
+        "MLIRLinalgToStandard",
+        "MLIRMathToLibm",
+        "MLIROpenMPToLLVM",
+        "MLIRPDLToPDLInterp",
+        "MLIRSCFToGPU",
+        "MLIRSCFToOpenMP",
+        "MLIRSCFToSPIRV",
+        "MLIRSCFToStandard",
+        "MLIRShapeToStandard",
+        "MLIRSPIRVToLLVM",
+        "MLIRStandardToLLVM",
+        "MLIRStandardToSPIRV",
+        "MLIRTosaToLinalg",
+        "MLIRTosaToSCF",
+        "MLIRTosaToStandard",
+        "MLIRVectorToROCDL",
+        "MLIRVectorToLLVM",
+        "MLIRVectorToSCF",
+        "MLIRVectorToSPIRV",
+        //translation_libs
+        "MLIRAffineToStandard",
+        "MLIRAsyncToLLVM",
+        "MLIRComplexToLLVM",
+        "MLIRGPUToGPURuntimeTransforms",
+        "MLIRGPUToNVVMTransforms",
+        "MLIRGPUToROCDLTransforms",
+        "MLIRGPUToSPIRV",
+        "MLIRGPUToVulkanTransforms",
+        "MLIRLinalgToLLVM",
+        "MLIRLinalgToSPIRV",
+        "MLIRLinalgToStandard",
+        "MLIRMathToLibm",
+        "MLIROpenMPToLLVM",
+        "MLIRPDLToPDLInterp",
+        "MLIRSCFToGPU",
+        "MLIRSCFToOpenMP",
+        "MLIRSCFToSPIRV",
+        "MLIRSCFToStandard",
+        "MLIRShapeToStandard",
+        "MLIRSPIRVToLLVM",
+        "MLIRStandardToLLVM",
+        "MLIRStandardToSPIRV",
+        "MLIRTosaToLinalg",
+        "MLIRTosaToSCF",
+        "MLIRTosaToStandard",
+        "MLIRVectorToROCDL",
+        "MLIRVectorToLLVM",
+        "MLIRVectorToSCF",
+        "MLIRVectorToSPIRV",
+        "MLIRAnalysis",
+        "MLIRCallInterfaces",
+        "MLIRExecutionEngine",
+        "MLIRIR",
+        "MLIRLLVMIR",
+        "MLIRParser",
+        "MLIRPass",
+        "MLIRSideEffectInterfaces",
+        "MLIRSupport",
+        "MLIRTargetLLVMIRExport",
+        "MLIRTransforms",
+        "MLIROptLib",
+        "rust_cpp_generated",
+    ]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![recursion_limit="256"]
+#![recursion_limit = "256"]
+#![feature(trait_alias)]
 
 use std::future::Future;
 
@@ -7,12 +8,10 @@ enum Operation {
     HasLabel(),
 }
 
-struct Has {
-
-}
+struct Has {}
 
 struct Traversal {
-    ops: Vec<Operation>, 
+    ops: Vec<Operation>,
 }
 
 impl Traversal {
@@ -23,7 +22,7 @@ impl Traversal {
     fn has_label() -> Self {
         todo!()
     }
-    
+
     fn faz_has(self) -> Self {
         todo!()
     }
@@ -33,22 +32,32 @@ impl Traversal {
     }
 }
 
-use cpp::cpp;
+use cpp::{cpp, cpp_class};
 
-cpp!{{
+cpp! {{
     #include <iostream>
     #include "mlir/IR/Dialect.h"
     #include "mlir/IR/BuiltinOps.h"
     #include "mlir/IR/MLIRContext.h"
     #include "mlir/IR/OpDefinition.h"
     #include "mlir/IR/BuiltinTypes.h"
+    #include "mlir/IR/AsmState.h"
+
     #include "mlir/Support/LogicalResult.h"
+    #include "mlir/Support/TypeID.h"
+    #include "mlir/ExecutionEngine/ExecutionEngine.h"
+    #include "mlir/ExecutionEngine/OptUtils.h"
+    #include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+    #include "mlir/Target/LLVMIR/Export.h"
+    #include "llvm/Support/TargetSelect.h"
 
     class RepeatOp;
 
     class MMADTDialect : public mlir::Dialect {
     public:
-        explicit MMADTDialect(mlir::MLIRContext *ctx);
+        explicit MMADTDialect(mlir::MLIRContext *ctx): mlir::Dialect("mmadt", ctx, mlir::TypeID::get<MMADTDialect>()) {
+
+        }
 
         static  llvm::StringRef getDialectNamespace() {
             return "mmadt";
@@ -66,27 +75,113 @@ cpp!{{
                         mlir::OpTrait::OneTypedResult<mlir::TensorType>::Impl> {
     public:
         using Op::Op;
-        static llvm::StringRef getOperationName() { return "toy.constant"; }
-        mlir::DenseElementsAttr getValue();
-        mlir::LogicalResult verify();
-        static void build(mlir::OpBuilder &builder, mlir::OperationState &state, mlir::Type result, mlir::DenseElementsAttr value);
-        static void build(mlir::OpBuilder &builder, mlir::OperationState &state, mlir::DenseElementsAttr value);
-        static void build(mlir::OpBuilder &builder, mlir::OperationState &state, double value);
+
+        static llvm::StringRef getOperationName() { return "mmadt.repeat"; }
+
+        // mlir::DenseElementsAttr getValue();
+
+        // mlir::LogicalResult verify();
+
+        static void build(
+            mlir::OpBuilder &builder,
+            mlir::OperationState &state,
+            mlir::Type result,
+            mlir::DenseElementsAttr value
+        ) {
+            // mlir::OpBuilder::create<ConstantOp>(...);
+        }
     };
 }}
 
-fn main() {
-    let name = std::ffi::CString::new("World").unwrap();
-    let name_ptr = name.as_ptr();
-    let r = unsafe {
-        cpp!([name_ptr as "const char *"] -> u32 as "int32_t" {
-            mlir::MLIRContext context;
-            // context.getOrLoadDialect<MMADTDialect>();
-            // mlir::OwningModuleRef module;
+cpp! {{
+    // int runJit(mlir::ModuleOp module) {
+    //     // Initialize LLVM targets.
+    //     llvm::InitializeNativeTarget();
+    //     llvm::InitializeNativeTargetAsmPrinter();
 
-            std::cout << "Hello, 12" << name_ptr << std::endl;
-            return 42;
-        })
+    //     // Register the translation from MLIR to LLVM IR, which must happen before we
+    //     // can JIT-compile.
+    //     mlir::registerLLVMDialectTranslation(*module->getContext());
+
+    //     // An optimization pipeline to use within the execution engine.
+    //     auto optPipeline = mlir::makeOptimizingTransformer(
+    //         /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+    //         /*targetMachine=*/nullptr);
+
+    //     // Create an MLIR execution engine. The execution engine eagerly JIT-compiles
+    //     // the module.
+    //     auto maybeEngine = mlir::ExecutionEngine::create(module, nullptr, optPipeline);
+    //     assert(maybeEngine && "failed to construct an execution engine");
+    //     auto &engine = maybeEngine.get();
+
+    //     // Invoke the JIT-compiled function.
+    //     auto invocationResult = engine->invokePacked("main");
+    //     if (invocationResult) {
+    //       llvm::errs() << "JIT invocation failed\n";
+    //       return -1;
+    //     }
+
+    //     return 0;
+    //   }
+}}
+
+enum Instruction {
+    Branch,
+    Combine,
+    Lift,
+    Merge,
+    Repeat,
+    Split,
+    Swap,
+    Trace,
+}
+
+trait Instructions = IntoIterator<Item = Instruction>;
+
+/// Implements of a simple MLIR emission.
+cpp! {{
+    struct MLIRGen {
+        mlir::MLIRContext context;
+        mlir::ModuleOp module;
     };
-    assert_eq!(r, 42)
+}}
+cpp_class!(pub unsafe struct MLIRGen as "MLIRGen");
+
+impl MLIRGen {
+    fn new() -> Self {
+        unsafe {
+            cpp!([] -> MLIRGen as "MLIRGen" {
+                mlir::MLIRContext context;
+                context.getOrLoadDialect<MMADTDialect>();
+                mlir::OpBuilder builder{context};
+                auto module = mlir::ModuleOp::create(builder.getUnknownLoc());
+
+                // mlir::OwningModuleRef module;
+                // if (int error = runJit(module)) {
+                //     std::cout << "error: " << error << std::endl;
+                // }
+
+                // std::cout << "Hello, 1212" << std::endl;
+                return MLIRGen{context, module};
+            })
+        }
+    }
+
+    // Translate `mmadt`
+    fn translate(&mut self, insts: impl Instructions) {
+        for inst in insts {}
+    }
+
+    fn dump(&self) {
+        unsafe {
+            cpp!([mut self as "MLIRGen"] {
+                return self.module.dump();
+            })
+        }
+    }
+}
+
+fn main() {
+    let gen = MLIRGen::new();
+    gen.dump();
 }
